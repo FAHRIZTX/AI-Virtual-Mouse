7e2425f2f2fe717b3030b215d19179b25f5c34a5
Muhammad Fahri <fahriztx77@gmail.com>
2021-10-29 22:56:17 +0700

    initial commits

--- AIVirtualMouse.py
+++ AIVirtualMouse.py
@@ -3,11 +3,13 @@ import numpy as np
 import HandTrackingModule as htm
 import time
 import autopy
+import pyautogui as pag
 
 ######################
 wCam, hCam = 640, 480
 frameR = 100     #Frame Reduction
 smoothening = 7  #random value
+hold = False
 ######################
 
 pTime = 0
@@ -20,57 +22,77 @@ cap.set(4, hCam)
 detector = htm.handDetector(maxHands=1)
 wScr, hScr = autopy.screen.size()
 
-# print(wScr, hScr)
 
 while True:
-    # Step1: Find the landmarks
     success, img = cap.read()
     img = detector.findHands(img)
     lmList, bbox = detector.findPosition(img)
 
-    # Step2: Get the tip of the index and middle finger
     if len(lmList) != 0:
         x1, y1 = lmList[8][1:]
         x2, y2 = lmList[12][1:]
 
-        # Step3: Check which fingers are up
         fingers = detector.fingersUp()
-        cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),
-                      (255, 0, 255), 2)
 
-        # Step4: Only Index Finger: Moving Mode
-        if fingers[1] == 1 and fingers[2] == 0:
+        if fingers[1] == 1 and fingers[4] == 0 and fingers[2] == 0:
 
-            # Step5: Convert the coordinates
             x3 = np.interp(x1, (frameR, wCam-frameR), (0, wScr))
             y3 = np.interp(y1, (frameR, hCam-frameR), (0, hScr))
 
-            # Step6: Smooth Values
             clocX = plocX + (x3 - plocX) / smoothening
             clocY = plocY + (y3 - plocY) / smoothening
 
-            # Step7: Move Mouse
             autopy.mouse.move(wScr - clocX, clocY)
             cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
             plocX, plocY = clocX, clocY
 
-        # Step8: Both Index and middle are up: Clicking Mode
-        if fingers[1] == 1 and fingers[2] == 1:
+        if fingers[1] == 1 and fingers[2] == 1 and fingers[0] == 0:
 
-            # Step9: Find distance between fingers
-            length, img, lineInfo = detector.findDistance(8, 12, img)
+            x3 = np.interp(x1, (frameR, wCam-frameR), (0, wScr))
+            y3 = np.interp(y1, (frameR, hCam-frameR), (0, hScr))
 
-            # Step10: Click mouse if distance short
-            if length < 40:
-                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
-                autopy.mouse.click()
+            clocX = plocX + (x3 - plocX) / smoothening
+            clocY = plocY + (y3 - plocY) / smoothening
 
-    # Step11: Frame rate
+            if clocY > 400:
+                pag.scroll(-100)
+
+            if clocY < 400:
+                pag.scroll(100)
+
+        if fingers[1] == 1 and fingers[0] == 0 and fingers[2] == 0:
+            if hold == True:
+                autopy.mouse.toggle(autopy.mouse.Button.LEFT, False)
+                hold = False
+
+        if fingers[1] == 1 and fingers[0] == 1 and fingers[2] == 0:
+
+            x3 = np.interp(x1, (frameR, wCam-frameR), (0, wScr))
+            y3 = np.interp(y1, (frameR, hCam-frameR), (0, hScr))
+
+            clocX = plocX + (x3 - plocX) / smoothening
+            clocY = plocY + (y3 - plocY) / smoothening
+
+            if hold == False:
+                autopy.mouse.toggle(autopy.mouse.Button.LEFT, True)
+                hold = True
+
+            autopy.mouse.move(wScr - clocX, clocY)
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            plocX, plocY = clocX, clocY
+
+        if fingers[1] == 1 and fingers[4] == 1 and fingers[2] == 0:
+
+            length, img, lineInfo = detector.findDistance(8, 20, img)
+
+            cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+            autopy.mouse.click()
+            time.sleep(0.10)
+
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
     cv2.putText(img, str(int(fps)), (28, 58), cv2.FONT_HERSHEY_PLAIN, 3, (255, 8, 8), 3)
 
-    # Step12: Display
     cv2.imshow("Image", img)
     cv2.waitKey(1)
--- README.md
+++ README.md
@@ -1,24 +1,40 @@
 ## AI Virtual Mouse
 
 > Developed an AI-based approach for controlling the mouse movement using Python and openCV with real-time camera that detects hand landmarks, tracks gesture patterns instead of a physical mouse.
+> 
+> This repository is based from [https://github.com/ravigithub19/ai-virtual-mouse](https://github.com/ravigithub19/ai-virtual-mouse) and i modified with add Scroll Mouse and Click Hold Mouse.
 
-## Dependencies  -
-> Please install all the required dependencies.
-* openCV - (For image processing and drawing)
-* mediapipe - (For Hand Tracking)
-* autopy - (For controlling the mouse movement and click)
-* numpy
+## Requirements
+
+- Git ([Download Here](https://git-scm.com/downloads))
+- Python 3.6 ([Download Here](https://www.python.org/downloads/release/python-368/))
 
 ## Installation
 
 To setup the system for development on your local machine, please follow the instructions below:
 
+Make sure you have installed python3 on your machine.
+
 1. Clone the repository to your machine
 
-   ```bash
-   git clone https://github.com/ravigithub19/ai-virtual-mouse.git
-   ```
+    ```bash
+    $ git clone https://github.com/FAHRIZTX/AI-Virtual-Mouse.git
+    $ cd AI-Virtual-Mouse
+    ```
 
-2. Run the ```AIVirtualMouse.py``` python file.
+2. Install all the required dependencies by Run
+
+    ```bash
+    $ python3 -m pip install -r requirements.txt
+    ```
+
+3. Run the python file.
+
+    ```bash
+    $ python3 AIVirtualMouse.py
+    ```
    
+## Credit
 
+> Thanks to:
+> [https://github.com/ravigithub19/ai-virtual-mouse](https://github.com/ravigithub19/ai-virtual-mouse)
